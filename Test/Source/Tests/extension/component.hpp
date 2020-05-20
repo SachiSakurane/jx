368acbd09f47400dee10eea6872a21e5aeee3a13
SachiSakurane <sakuranesachi@gmail.com>
2020-05-20 22:52:30 +0900

    add test

--- Test/Source/Tests/extension/component.hpp
+++ Test/Source/Tests/extension/component.hpp
@@ -8,6 +8,8 @@ class RXComponentTest : public UnitTest {
 public:
     RXComponentTest() : UnitTest("RXComponentTest") {}
 
+    class SubComponent : public Component {};
+
     void runTest() override {
         beginTest("Visibility Test");
         {
@@ -48,6 +50,42 @@ public:
 
             Rectangle<int> bounds1 {100, 50, 400, 200};
             jx::RX<Component> component;
+            component.rx.bounds().subscribe([this](const auto& b) {
+                logMessage("subject bounds changed: " + b.toString());
+            }) | jx::disposed(bag);
+            component.setBounds(bounds1);
+
+            Rectangle<int> bounds2 {400, 500, 4000, 2100};
+            component.setBounds(bounds2);
+        }
+
+        beginTest("SubComponent Test");
+        {
+            jx::dispose_bag bag;
+            jx::RX<SubComponent> component;
+
+            component.rx.visibility().subscribe([this](const bool& b){
+                logMessage("subject visibility changed: " + std::to_string(b));
+            }) | jx::disposed(bag);
+
+            component.setVisible(true);
+            expect(component.isVisible());
+            component.setVisible(false);
+            expect(!component.isVisible());
+
+            component.rx.mouse(jx::MouseEventType::kDown).subscribe([](const auto&){}) | jx::disposed(bag);
+            component.rx.mouse(jx::MouseEventType::kDrag).subscribe([](const auto&){}) | jx::disposed(bag);
+            component.rx.mouse(jx::MouseEventType::kDoubleClick).subscribe([](const auto&){}) | jx::disposed(bag);
+            component.rx.mouse(jx::MouseEventType::kUp).subscribe([](const auto&){}) | jx::disposed(bag);
+            component.rx.mouse(jx::MouseEventType::kExit).subscribe([](const auto&){}) | jx::disposed(bag);
+            component.rx.mouse(jx::MouseEventType::kMove).subscribe([](const auto&){}) | jx::disposed(bag);
+            component.rx.mouse(jx::MouseEventType::kEnter).subscribe([](const auto&){}) | jx::disposed(bag);
+
+            component.rx.wheelMove().subscribe([](const auto&){}) | jx::disposed(bag);
+
+            component.rx.magnify().subscribe([](const auto&){}) | jx::disposed(bag);
+
+            Rectangle<int> bounds1 {100, 50, 400, 200};
             component.rx.bounds().subscribe([this](const auto& b) {
                 logMessage("subject bounds changed: " + b.toString());
             }) | jx::disposed(bag);
